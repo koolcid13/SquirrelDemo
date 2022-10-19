c05ea248bd8d495ec32d8e1abd403a16c0e0b959
koolcid13 <avin.ahmadi23@ycdsbk12.ca>
2022-10-19 10:55:42 -0400

    demo complete

--- src/OOPDemo/SquirrelDemo.java
+++ src/OOPDemo/SquirrelDemo.java
@@ -1,3 +1,30 @@
+package OOPDemo;
+import OOPDemo.Squirrel;
+
 public class SquirrelDemo {
     
+    public static void main(String[] args) {
+        
+        Squirrel alvin;
+        Squirrel simon;
+        Squirrel theo;
+
+        alvin = new Squirrel("Red", "Medium", 8, 100);
+        simon = new Squirrel("Blue", "Tall", 2, 240);
+        theo = new Squirrel("Green", "Grande", 3, 90);
+
+
+        alvin.climb();
+        simon.findNut(3);
+        theo.eatNut();
+        theo.findNut(23);
+        alvin.run();
+        System.out.println ("theo's nut count: " + theo.getNutCount());
+        simon.run();
+        simon.kill();
+
+        System.out.println ("simon's iq: " + simon.iqDouble);
+
+    }
+
 }
